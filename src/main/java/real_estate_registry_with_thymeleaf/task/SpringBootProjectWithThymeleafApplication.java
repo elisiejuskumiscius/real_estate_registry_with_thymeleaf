aa74bbb24167e3e5e2039a6affc8a9ecdfc358dc
elisiejus <ekumiscius@gmail.com>
2020-11-02 17:24:11 +0200

    Initial Spring commit

---
+++ src/main/java/real_estate_registry_with_thymeleaf/task/SpringBootProjectWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package real_estate_registry_with_thymeleaf.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectWithThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/real_estate_registry_with_thymeleaf/task/SpringBootProjectWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package real_estate_registry_with_thymeleaf.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectWithThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
